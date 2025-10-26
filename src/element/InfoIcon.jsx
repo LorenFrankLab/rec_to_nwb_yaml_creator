6948ce2f92a6f00f8c3e898641a7619564a110c1
Eric Denovellis <edeno@bu.edu>
2025-10-25 20:16:59 -0400

    Fix InfoIcon to use imported faCircleInfo icon
    Replaces the string-based icon reference with the imported faCircleInfo object from FontAwesome. This ensures compatibility with the FontAwesome React component and prevents icon rendering issues.

--- src/element/InfoIcon.jsx
+++ src/element/InfoIcon.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 
 
 /**
@@ -14,7 +15,7 @@ const InfoIcon = (prop) => {
 
   return (
     <span title={infoText}>
-      <FontAwesomeIcon icon="circle-info" size={size} className="info-icon" />
+      <FontAwesomeIcon icon={faCircleInfo} size={size} className="info-icon" />
     </span>
   );
 };
